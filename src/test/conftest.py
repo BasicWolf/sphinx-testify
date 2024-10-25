e1d34879b136d76b26602c1344f42dbd062f1f10
Zaur Nasibov <1289161+BasicWolf@users.noreply.github.com>
2024-10-25 22:29:10 +0300

    Use test fixtures and helpers to simplify test body

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,5 +1,9 @@
-import pytest
+from __future__ import annotations
+
 from pathlib import Path
+
+import pytest
+from sphinx.testing.util import SphinxTestApp
 
 pytest_plugins = ('sphinx.testing.fixtures',)
 
@@ -10,3 +14,28 @@ collect_ignore = ['fixtures']
 @pytest.fixture(scope='session')
 def rootdir() -> Path:
     return Path(__file__).parent.resolve() / 'fixtures'
+
+
+@pytest.fixture
+def test_app(app: SphinxTestApp) -> TestifySphinxTestApp:
+    return TestifySphinxTestApp(app)
+
+
+class TestifySphinxTestApp:
+    __test__ = False
+    _app: SphinxTestApp
+    _testified: list[str]
+
+    def __init__(self, app: SphinxTestApp):
+        self._app = app
+        self._testified = []
+        self._app.connect('testify-testified', self._on_testified)
+
+    def _on_testified(self, _app, test_name: str):
+        self._testified.append(test_name)
+
+    def build(self):
+        self._app.build(filenames=[str(self._app.srcdir / 'index.rst')])
+
+    def has_testified(self, test_name) -> bool:
+        return test_name in self._testified
--- src/test/test_sphinx_testify.py
+++ src/test/test_sphinx_testify.py
@@ -1,17 +1,12 @@
 import pytest
-from sphinx.testing.util import SphinxTestApp
+
+from test.conftest import TestifySphinxTestApp
 
 
 @pytest.mark.sphinx('html', testroot='single-passed-test')
-def test_testify_single_passed_test_case(app: SphinxTestApp):
-    testified_only_registered_users_have_access = False
+def test_testify_single_passed_test_case(test_app: TestifySphinxTestApp):
+    test_app.build()
 
-    def _on_testified(_app, test_name: str):
-        nonlocal testified_only_registered_users_have_access
-        if test_name == 'testsuite.testclass.test_only_registered_users_have_access':  # noqa E501 - line too long
-            testified_only_registered_users_have_access = True
-    app.connect('testify-testified', _on_testified)
-
-    app.build(filenames=[str(app.srcdir / 'index.rst')])
-
-    assert testified_only_registered_users_have_access
+    assert test_app.has_testified(
+        'testsuite.testclass.test_only_registered_users_have_access'
+    )
