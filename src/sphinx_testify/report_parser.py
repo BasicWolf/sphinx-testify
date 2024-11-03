f7fb668a03b048f6f617ee0a065c1ae719a51ecc
Zaur Nasibov <1289161+BasicWolf@users.noreply.github.com>
2024-11-03 19:16:17 +0200

    Refactor for clarity
    Extract report failures parsing logic to functions.

--- src/sphinx_testify/report_parser.py
+++ src/sphinx_testify/report_parser.py
@@ -1,4 +1,5 @@
 from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
 
 from sphinx.util.logging import getLogger
 from sphinx_testify.test_result import TestFailure, TestResult, TestResults
@@ -26,19 +27,25 @@ def parse_tests_results_xml(testify_from: str | list[str]) -> TestResults:
                 test_class_name = testcase_elem.get('classname')
                 test_name = testcase_elem.get('name')
 
-                failures = [
-                    TestFailure(
-                        message=failure_elem.get('message', ''),
-                        type=failure_elem.get('type', '')
-                    )
-                    for failure_elem in testcase_elem.iterfind('failure')
-                ]
-
                 test_results.add(
                     TestResult(
                         name=f'{testsuite_name}.{test_class_name}.{test_name}',
-                        failures=failures
+                        failures=_parse_failures(testcase_elem)
                     )
                 )
 
     return test_results
+
+
+def _parse_failures(testcase_elem):
+    return [
+        _parse_failure(failure_elem)
+        for failure_elem in testcase_elem.iterfind('failure')
+    ]
+
+
+def _parse_failure(elem: Element) -> TestFailure:
+    return TestFailure(
+        message=elem.get('message', ''),
+        type=elem.get('type', '')
+    )
