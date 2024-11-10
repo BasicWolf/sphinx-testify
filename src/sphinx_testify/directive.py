4e101d71190f271df69e6b1c5c7d732f0897738a
Zaur Nasibov <1289161+BasicWolf@users.noreply.github.com>
2024-11-10 22:19:08 +0200

    Reformat license headers in files to fit into 80 characters
    Have to conform with PEP8

--- src/sphinx_testify/directive.py
+++ src/sphinx_testify/directive.py
@@ -1,5 +1,6 @@
 # Copyright: (c) 2024, Zaur Nasibov <zaur@zaurnasibov.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+
+# See COPYING or https://www.gnu.org/licenses/gpl-3.0.txt
 
 from docutils import nodes
 from sphinx.application import Sphinx
