31ede34568fc7b73ca5687ba353daba952f2311c
Zaur Nasibov <1289161+BasicWolf@users.noreply.github.com>
2024-12-22 16:43:52 +0200

    Add copyright headers

--- tests/test_test_result.py
+++ tests/test_test_result.py
@@ -1,3 +1,7 @@
+# Copyright: (c) 2024, Zaur Nasibov <zaur@zaurnasibov.com>
+# GNU General Public License v3.0+
+# See COPYING or https://www.gnu.org/licenses/gpl-3.0.txt
+
 import pytest
 
 from sphinx_testify.test_result import TestFailure, TestResult
